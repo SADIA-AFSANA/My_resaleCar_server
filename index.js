0a1a3f424214eb4e6d31d5881bc9a5a433c3e980
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-11-26 08:18:53 +0600

    mongodb set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.jlfv4.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 
 
